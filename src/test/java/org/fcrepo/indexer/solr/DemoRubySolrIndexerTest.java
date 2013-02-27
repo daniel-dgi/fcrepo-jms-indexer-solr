48ef94ca20dce67a304d60b6e142ee74b261631d
ajs6f <ajs6f@virginia.edu>
2013-02-27 15:19:34 -0500

    Tightened test matcher

--- src/test/java/org/fcrepo/indexer/solr/DemoRubySolrIndexerTest.java
+++ src/test/java/org/fcrepo/indexer/solr/DemoRubySolrIndexerTest.java
@@ -1,4 +1,21 @@
+
 package org.fcrepo.indexer.solr;
+
+import static com.google.common.collect.Iterators.all;
+import static javax.jms.Session.AUTO_ACKNOWLEDGE;
+import static org.apache.abdera.model.Text.Type.TEXT;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.inject.Inject;
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 
 import org.apache.abdera.Abdera;
 import org.apache.abdera.model.Entry;
@@ -9,7 +26,6 @@ import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.SolrInputField;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,27 +35,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.inject.Inject;
-import javax.jms.Connection;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-
-import static javax.jms.Session.AUTO_ACKNOWLEDGE;
-import static org.apache.abdera.model.Text.Type.TEXT;
-import static org.mockito.Mockito.*;
+import com.google.common.base.Predicate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/spring-test/solr-indexer.xml"})
 public class DemoRubySolrIndexerTest {
 
-    final private Logger logger = LoggerFactory.getLogger(DemoRubySolrIndexerTest.class);
-
+    final private Logger logger = LoggerFactory
+            .getLogger(DemoRubySolrIndexerTest.class);
 
     private Connection connection;
 
@@ -57,15 +60,18 @@ public class DemoRubySolrIndexerTest {
         return getTestMessage(session, "addDatastream");
     }
 
-    private static Message getDeleteMessage(Session session) throws JMSException {
+    private static Message getDeleteMessage(Session session)
+            throws JMSException {
         return getTestMessage(session, "purgeDatastream");
     }
 
-    private static Message getTestMessage(Session session, String operation) throws JMSException {
+    private static Message getTestMessage(Session session, String operation)
+            throws JMSException {
         Abdera abdera = new Abdera();
 
         Entry entry = abdera.newEntry();
-        entry.setTitle(operation, TEXT).setBaseUri("http://localhost:8080/rest");
+        entry.setTitle(operation, TEXT)
+                .setBaseUri("http://localhost:8080/rest");
         entry.addCategory("xsd:string", TEST_PID, "fedora-types:pid");
         entry.setContent("contentds");
         StringWriter writer = new StringWriter();
@@ -80,7 +86,8 @@ public class DemoRubySolrIndexerTest {
     }
 
     @Test
-    public void testSimpleScriptingSolrIndexer() throws JMSException, IOException, SolrServerException {
+    public void testSimpleScriptingSolrIndexer() throws JMSException,
+            IOException, SolrServerException {
         Message m = getAddMessage(session);
 
         SolrServer s = mock(SolrServer.class);
@@ -92,7 +99,6 @@ public class DemoRubySolrIndexerTest {
         indexer.onMessage(m);
         verify(s).add(argThat(new MatchesSolrDocument(doc)));
     }
-
 
     @Before
     public void acquireConnection() throws JMSException {
@@ -110,6 +116,7 @@ public class DemoRubySolrIndexerTest {
     }
 
     private class MatchesSolrDocument extends BaseMatcher<SolrInputDocument> {
+
         private final SolrInputDocument source_doc;
 
         public MatchesSolrDocument(SolrInputDocument doc) {
@@ -117,43 +124,19 @@ public class DemoRubySolrIndexerTest {
         }
 
         @Override
-        public boolean matches(Object o) {
-            final SolrInputDocument test_doc = (SolrInputDocument) o;
+        public boolean matches(final Object o) {
 
-            final Iterator<SolrInputField> it = source_doc.iterator();
+            return all(source_doc.iterator(),
+                    new Predicate<SolrInputField>() {
 
-            while(it.hasNext()) {
-                final SolrInputField source_field = it.next();
-                final SolrInputField test_field = test_doc.getField(source_field.getName());
+                        public boolean apply(SolrInputField f) {
+                            return ((SolrInputDocument) o)
+                                    .getField(f.getName()).getValues()
+                                    .containsAll(f.getValues());
+                        }
+                    });
 
-                Collection<String> source_values = new ArrayList<String>();
-                Collection<String> test_values = new ArrayList<String>();
-
-
-                final Iterator<Object> fieldIterator = source_field.getValues().iterator();
-
-                while(fieldIterator.hasNext()) {
-                    source_values.add((String)fieldIterator.next());
-                }
-
-                final Iterator<Object> testFieldIterator = test_field.getValues().iterator();
-
-                while(testFieldIterator.hasNext()) {
-                    test_values.add((String)testFieldIterator.next());
-                }
-
-
-                if(!test_values.containsAll(source_values)) {
-                    System.out.println(source_field.getName() + " did not match!");
-                    System.out.println("Source:" + source_values.toString());
-                    System.out.println("Test:" + test_values.toString());
-                    return false;
-                }
-            }
-
-            return true;
         }
-
 
         @Override
         public void describeTo(Description description) {
